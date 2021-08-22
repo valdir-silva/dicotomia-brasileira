62ee2c25bf5ab72c4565a27a6d1d420c2b484717
Valdir Silva <whiteofdark@outlook.com>
2021-08-22 19:37:19 -0300

    criando viewBinding

--- app/src/main/java/net/alunando/dicotomiabrasileira/ui/MainActivity.kt
+++ app/src/main/java/net/alunando/dicotomiabrasileira/ui/MainActivity.kt
@@ -2,13 +2,10 @@ package net.alunando.dicotomiabrasileira.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import net.alunando.dicotomiabrasileira.ui.home.HomeFragment
 
 open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-
-        intent.action to HomeFragment
     }
 }
--- app/src/main/java/net/alunando/dicotomiabrasileira/ui/home/HomeFragment.kt
+++ app/src/main/java/net/alunando/dicotomiabrasileira/ui/home/HomeFragment.kt
@@ -6,16 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import net.alunando.dicotomiabrasileira.R
+import net.alunando.dicotomiabrasileira.databinding.HomeFragmentBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by viewModel()
     private lateinit var binding: HomeFragmentBinding
-
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,4 +21,18 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViewModel()
+        setupBinding()
+    }
+
+    private fun setupViewModel() {
+        
+    }
+
+    private fun setupBinding() {
+
+    }
 }
