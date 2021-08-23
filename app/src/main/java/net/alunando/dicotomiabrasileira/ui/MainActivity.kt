130657848049cbde878451d7f60e23f6978c7fec
Valdir Silva <whiteofdark@outlook.com>
2021-08-22 21:06:01 -0300

    implementando Navigation

--- app/src/main/java/net/alunando/dicotomiabrasileira/ui/MainActivity.kt
+++ app/src/main/java/net/alunando/dicotomiabrasileira/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package net.alunando.dicotomiabrasileira.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import net.alunando.dicotomiabrasileira.R
 
 open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
     }
 }
